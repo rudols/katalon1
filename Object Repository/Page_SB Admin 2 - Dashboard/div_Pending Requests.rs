c671ec581c797d3e59c86328211755e54c6546f7
Efran Pasaribu <'efranr@xl.co.id'>
2024-06-28 12:20:34 +0700

    test using tunnel

---
+++ Object Repository/Page_SB Admin 2 - Dashboard/div_Pending Requests.rs
@@ -0,0 +1,145 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Pending Requests</name>
+   <tag></tag>
+   <elementGuidId>8bdc0e01-e8b4-4191-bb4b-6e058681ea33</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>div.card.border-left-warning.shadow.h-100.py-2</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='content']/div/div[2]/div[4]/div</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>div:nth-child(4) > .card >> nth=0</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>7cbd16a7-e28a-4dd8-80a5-0377a3faaba6</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>card border-left-warning shadow h-100 py-2</value>
+      <webElementGuid>c397fe90-f6ae-4cad-8932-e441471534cc</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+                                
+                                    
+                                        
+                                            
+                                                Pending Requests
+                                            18
+                                        
+                                        
+                                            
+                                        
+                                    
+                                
+                            </value>
+      <webElementGuid>91f18a59-1235-41b4-a757-40ba1e8b842c</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;content&quot;)/div[@class=&quot;container-fluid&quot;]/div[@class=&quot;row&quot;]/div[@class=&quot;col-xl-3 col-md-6 mb-4&quot;]/div[@class=&quot;card border-left-warning shadow h-100 py-2&quot;]</value>
+      <webElementGuid>1f4a752e-01ff-4ccd-83bf-0f2c4add61dd</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='content']/div/div[2]/div[4]/div</value>
+      <webElementGuid>daf5ca52-90f1-4550-a6ca-2342f3956279</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Tasks'])[1]/following::div[9]</value>
+      <webElementGuid>d9a5973b-eb81-4687-b8db-98fdb15982a4</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='$215,000'])[1]/following::div[16]</value>
+      <webElementGuid>2ae2dc0e-36ee-4768-bed6-dad251b43c81</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Earnings Overview'])[1]/preceding::div[7]</value>
+      <webElementGuid>a52ea99a-0175-4bb1-8e3f-f99fb7ce7ea9</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/div[4]/div</value>
+      <webElementGuid>72adc716-53ff-43d4-85f8-a1e72a01c460</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = '
+                                
+                                    
+                                        
+                                            
+                                                Pending Requests
+                                            18
+                                        
+                                        
+                                            
+                                        
+                                    
+                                
+                            ' or . = '
+                                
+                                    
+                                        
+                                            
+                                                Pending Requests
+                                            18
+                                        
+                                        
+                                            
+                                        
+                                    
+                                
+                            ')]</value>
+      <webElementGuid>56467a4f-ed78-4a08-b6eb-e22e2985d424</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
